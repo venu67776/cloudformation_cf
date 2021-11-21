7a9a3e9167e3de4620343a0e9a4156a7c778a2bf
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-11-21 21:20:08 +0530

    new

--- env/core_svc_dev/env.sh
+++ env/core_svc_dev/env.sh
@@ -7,3 +7,11 @@
 
 - ParameterKey: Password
   ParameterValue: venu@1234
+
+---
+- ParameterKey: FunctionName
+  ParameterValue: taskaplambda
+- ParameterKey: MemorySize
+  ParameterValue: '512'
+- ParameterKey: Timeout
+  ParameterValue: '5'
