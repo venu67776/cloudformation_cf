ded6b25a88abb57edd96407dc5a751cf74c7001d
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-11-21 13:48:20 +0530

    new

---
+++ env/core_svc_dev/s3bucket/deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+STACK_NAME=$1
+PASSWORD=$2
+
+if [ -z "$1" ]
+  then
+    echo "No STACK_NAME argument"
+    exit 1
+fi
+
+DIR="$(cd $(dirname $BASH_SOURCE) && pwd)"
+
+
+echo "Creating stack..."
+  aws cloudformation create-stack \
+  --stack-name ${STACK_NAME} \
+  --template-body file:///home/ubuntu/cloudformation_cf/lab/s3.yaml \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameters ParameterKey=Username,ParameterValue=venu-aws \
+    	ParameterKey=Bucketname,ParameterValue=venuvenu \
+      ParameterKey=Password,ParameterValue=${PASSWORD} 
+    
+
+
+
+
