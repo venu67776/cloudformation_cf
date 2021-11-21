ab96bab6ccdde9b14706c2d750c1db724669fbec
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-11-21 06:54:52 +0530

    new

--- lab/validatetemplate.sh
+++ lab/validatetemplate.sh
@@ -4,4 +4,4 @@ DIR="$(cd $(dirname $BASH_SOURCE) && pwd)"
 
 echo "validating stack..."
 aws cloudformation validate-template \
---template-body file://${DIR}/*.yaml 
+--template-body file://${DIR}/s3.yaml 
