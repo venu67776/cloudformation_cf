545ca2bcf0db2b0acbe5ca7c9447abf0ee47a7a8
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-11-23 16:05:19 +0530

    new

--- env/core_svc_dev/s3bucket/deploy.sh
+++ env/core_svc_dev/s3bucket/deploy.sh
@@ -4,9 +4,11 @@
 mydir=$(cd ../../../lab && pwd)
 echo $mydir
 
+
 echo "Creating stack..."
   aws cloudformation create-stack \
   --stack-name ${Stackname} \
+  #--template-body file://C:/Users/VENUGOPAL/Desktop/training/cloudformation_cf/lab/s3.yaml \
   --template-body file://$mydir/s3.yaml \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameters ParameterKey=Username,ParameterValue=${Username} \
